7aeadc41d5b669c62cef08964c5f216e129c7698
James East <jameseast725@gmail.com>
2024-11-07 12:22:31 +0000

    adding the finishing code

--- src/App.js
+++ src/App.js
@@ -5,15 +5,33 @@ import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
 
 function App() {
-  /*
-  Define state variables for 
-  contacts and appointments 
-  */
+// Define state variables for contacts and appointments 
+  const [appointments, setAppointments] = useState([]);
+  const [contacts, setContacts] = useState([]);
 
-  /*
-  Implement functions to add data to
-  contacts and appointments
-  */
+// Implement functions to add data to contacts and appointments
+  const addAppointment = (name, contact, date, time) => {
+    setAppointments([
+      ...appointments,
+      {
+        name: name,
+        contact: contact,
+        date: date,
+        time: time,
+      },
+    ]);
+  };
+
+  const addContact = (name, phone, email) => {
+    setContacts([
+      ...contacts,
+      {
+        name: name,
+        phone: phone,
+        email: email,
+      },
+    ]);
+  };
 
   const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={ <Root/> }>
